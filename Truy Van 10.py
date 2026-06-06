aab4768a728d4f3c6f2b97dfab8037b670aae189
giamann <120298287+giamann@users.noreply.github.com>
2026-06-07 03:13:26 +0700

    Source code Truy Vấn 10 _Chương 4 _ 4.2

---
+++ Truy Van 10.py
@@ -0,0 +1,135 @@
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder\
+    .appName("Query_10")\
+    .getOrCreate()
+
+customers = spark.read.csv(
+    "hdfs://localhost:9000/Olist/olist_customers_dataset.csv",
+    header=True,
+    inferSchema=True
+)
+
+geolocation = spark.read.csv(
+    "hdfs://localhost:9000/Olist/olist_geolocation_dataset.csv",
+    header=True,
+    inferSchema=True
+)
+
+order_items = spark.read.csv(
+    "hdfs://localhost:9000/Olist/olist_order_items_dataset.csv",
+    header=True,
+    inferSchema=True
+)
+
+payments = spark.read.csv(
+    "hdfs://localhost:9000/Olist/olist_order_payments_dataset.csv",
+    header=True,
+    inferSchema=True
+)
+
+reviews = spark.read.csv(
+    "hdfs://localhost:9000/Olist/olist_order_reviews_dataset.csv",
+    header=True,
+    inferSchema=True
+)
+
+orders = spark.read.csv(
+    "hdfs://localhost:9000/Olist/olist_orders_dataset.csv",
+    header=True,
+    inferSchema=True
+)
+
+products = spark.read.csv(
+    "hdfs://localhost:9000/Olist/olist_products_dataset.csv",
+    header=True,
+    inferSchema=True
+)
+
+sellers = spark.read.csv(
+    "hdfs://localhost:9000/Olist/olist_sellers_dataset.csv",
+    header=True,
+    inferSchema=True
+)
+
+translation = spark.read.csv(
+    "hdfs://localhost:9000/Olist/product_category_name_translation.csv",
+    header=True,
+    inferSchema=True
+)
+customers.createOrReplaceTempView("customers")
+geolocation.createOrReplaceTempView("geolocation")
+order_items.createOrReplaceTempView("order_items")
+payments.createOrReplaceTempView("payments")
+reviews.createOrReplaceTempView("reviews")
+orders.createOrReplaceTempView("orders")
+products.createOrReplaceTempView("products")
+sellers.createOrReplaceTempView("sellers")
+translation.createOrReplaceTempView("translation")
+
+ #Truy vấn 10
+Truy_van_10 = """
+WITH monthly_product AS
+(
+    SELECT
+        oi.product_id,
+        DATE_FORMAT(
+            o.order_purchase_timestamp,
+            'yyyy-MM'
+        ) AS month,
+        SUM(oi.price) AS revenue
+    FROM orders o
+    JOIN order_items oi
+        ON o.order_id = oi.order_id
+    GROUP BY
+        oi.product_id,
+        DATE_FORMAT(
+            o.order_purchase_timestamp,
+            'yyyy-MM'
+        )
+)
+SELECT
+    product_id,
+    month,
+    revenue,
+    revenue - LAG(revenue)
+    OVER(
+        PARTITION BY product_id
+        ORDER BY month
+    ) AS growth
+FROM monthly_product
+"""
+ket_qua = spark.sql(Truy_van_10)
+ket_qua.show(100, False)
+
+#Trực quan hóa dữ liệu truy vấn 10
+import matplotlib.pyplot as plt
+df10 = ket_qua.toPandas()
+top_products = (
+    df10.groupby("product_id")["revenue"]
+    .sum()
+    .sort_values(ascending=False)
+    .head(5)
+    .index
+)
+df10 = df10[
+    df10["product_id"].isin(top_products)
+]
+plt.figure(figsize=(12,6))
+for product in top_products:
+    temp = df10[
+        df10["product_id"] == product
+    ]
+    plt.plot(
+        temp["month"],
+        temp["revenue"],
+        marker="o",
+        label=product[:8]
+    )
+plt.legend()
+plt.xticks(rotation=45)
+plt.title(
+    "Xu hướng doanh thu của Top 5 sản phẩm"
+)
+plt.tight_layout()
+plt.show()
