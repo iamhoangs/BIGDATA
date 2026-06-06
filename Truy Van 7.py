4dd252399295a5421291b7d89d1c6126ff3e6a12
giamann <120298287+giamann@users.noreply.github.com>
2026-06-07 03:12:23 +0700

    Source code Truy Vấn 7 _Chương 4 _ 4.2

---
+++ Truy Van 7.py
@@ -0,0 +1,118 @@
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder\
+    .appName("Query_7")\
+    .getOrCreate()
+
+customers = spark.read.csv(
+    "hdfs://localhost:9000/Olist/olist_customers_dataset.csv",
+    header=True,
+    inferSchema=True
+)
+
+geolocation = spark.read.csv(
+    "hdfs://localhost:9000/Olist/olist_geolocation_dataset.csv",
+    header=True,
+    inferSchema=True
+)
+
+order_items = spark.read.csv(
+    "hdfs://localhost:9000/Olist/olist_order_items_dataset.csv",
+    header=True,
+    inferSchema=True
+)
+
+payments = spark.read.csv(
+    "hdfs://localhost:9000/Olist/olist_order_payments_dataset.csv",
+    header=True,
+    inferSchema=True
+)
+
+reviews = spark.read.csv(
+    "hdfs://localhost:9000/Olist/olist_order_reviews_dataset.csv",
+    header=True,
+    inferSchema=True
+)
+
+orders = spark.read.csv(
+    "hdfs://localhost:9000/Olist/olist_orders_dataset.csv",
+    header=True,
+    inferSchema=True
+)
+
+products = spark.read.csv(
+    "hdfs://localhost:9000/Olist/olist_products_dataset.csv",
+    header=True,
+    inferSchema=True
+)
+
+sellers = spark.read.csv(
+    "hdfs://localhost:9000/Olist/olist_sellers_dataset.csv",
+    header=True,
+    inferSchema=True
+)
+
+translation = spark.read.csv(
+    "hdfs://localhost:9000/Olist/product_category_name_translation.csv",
+    header=True,
+    inferSchema=True
+)
+customers.createOrReplaceTempView("customers")
+geolocation.createOrReplaceTempView("geolocation")
+order_items.createOrReplaceTempView("order_items")
+payments.createOrReplaceTempView("payments")
+reviews.createOrReplaceTempView("reviews")
+orders.createOrReplaceTempView("orders")
+products.createOrReplaceTempView("products")
+sellers.createOrReplaceTempView("sellers")
+translation.createOrReplaceTempView("translation")
+
+ #Truy vấn 7
+Truy_van_7 = """
+SELECT
+    c.customer_state,
+    COUNT(*) AS total_orders,
+    SUM(
+        CASE
+            WHEN order_delivered_customer_date >
+                 order_estimated_delivery_date
+            THEN 1
+            ELSE 0
+        END
+    ) AS late_orders,
+    ROUND(
+        100.0 *
+        SUM(
+            CASE
+                WHEN order_delivered_customer_date >
+                     order_estimated_delivery_date
+                THEN 1
+                ELSE 0
+            END
+        ) /
+        COUNT(*),
+        2
+    ) AS late_rate
+FROM orders o
+JOIN customers c
+    ON o.customer_id = c.customer_id
+GROUP BY c.customer_state
+ORDER BY late_rate DESC
+"""
+ket_qua = spark.sql(Truy_van_7)
+ket_qua.show(100, False)
+
+#Trực quan hóa dữ liệu truy vấn 7
+import matplotlib.pyplot as plt
+df7 = ket_qua.toPandas()
+
+plt.figure(figsize=(12,6))
+plt.bar(
+    df7["customer_state"],
+    df7["late_rate"]
+)
+plt.title(
+    "Tỷ lệ giao hàng trễ theo bang"
+)
+plt.tight_layout()
+plt.show()
