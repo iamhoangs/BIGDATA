7ed885ae9b0988b17dcad223cb74c72db978d437
-iamhoangs- <hoangly.31221020414@st.ueh.edu.vn>
2026-06-07 02:16:56 +0700

    Source Code Truy Vấn5 _ Chương 4 _ 4.2

---
+++ TruyVan5.py
@@ -0,0 +1,114 @@
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder\
+    .appName("Truy_van_5")\
+    .getOrCreate()
+
+customers = spark.read.csv(
+    "hdfs://localhost:9000/Olist/olist_customers_dataset.csv",
+    header=True,
+    inferSchema=True
+)
+
+geolocation = spark.read.csv(
+    "hdfs://localhost:9000/Olist/olist_geolocation_dataset.csv",
+    header=True,
+    inferSchema=True
+)
+
+order_items = spark.read.csv(
+    "hdfs://localhost:9000/Olist/olist_order_items_dataset.csv",
+    header=True,
+    inferSchema=True
+)
+
+payments = spark.read.csv(
+    "hdfs://localhost:9000/Olist/olist_order_payments_dataset.csv",
+    header=True,
+    inferSchema=True
+)
+
+reviews = spark.read.csv(
+    "hdfs://localhost:9000/Olist/olist_order_reviews_dataset.csv",
+    header=True,
+    inferSchema=True
+)
+
+orders = spark.read.csv(
+    "hdfs://localhost:9000/Olist/olist_orders_dataset.csv",
+    header=True,
+    inferSchema=True
+)
+
+products = spark.read.csv(
+    "hdfs://localhost:9000/Olist/olist_products_dataset.csv",
+    header=True,
+    inferSchema=True
+)
+
+sellers = spark.read.csv(
+    "hdfs://localhost:9000/Olist/olist_sellers_dataset.csv",
+    header=True,
+    inferSchema=True
+)
+
+translation = spark.read.csv(
+    "hdfs://localhost:9000/Olist/product_category_name_translation.csv",
+    header=True,
+    inferSchema=True
+)
+customers.createOrReplaceTempView("customers")
+geolocation.createOrReplaceTempView("geolocation")
+order_items.createOrReplaceTempView("order_items")
+payments.createOrReplaceTempView("payments")
+reviews.createOrReplaceTempView("reviews")
+orders.createOrReplaceTempView("orders")
+products.createOrReplaceTempView("products")
+sellers.createOrReplaceTempView("sellers")
+translation.createOrReplaceTempView("translation")
+
+#Truy vấn 5
+Truy_van_5 = """
+WITH customer_value AS
+(
+   SELECT
+       c.customer_unique_id,
+       SUM(p.payment_value) AS spending
+   FROM customers c
+   JOIN orders o
+       ON c.customer_id = o.customer_id
+   JOIN payments p
+       ON o.order_id = p.order_id
+   GROUP BY
+       c.customer_unique_id
+)
+SELECT
+   customer_unique_id,
+   spending,
+   CASE
+       WHEN spending >= 1000
+           THEN 'VIP'
+       WHEN spending >= 500
+           THEN 'Regular'
+       ELSE 'Normal'
+   END AS customer_group
+FROM customer_value
+"""
+ket_qua_5 = spark.sql(Truy_van_5)
+ket_qua_5.show(100, False)
+#Trực quan hóa dữ liệu truy vấn 5
+import matplotlib.pyplot as plt
+df5 = ket_qua_5.toPandas()
+group_data = df5.groupby(
+    "customer_group"
+).size()
+plt.figure(figsize=(8,8))
+plt.pie(
+    group_data,
+    labels=group_data.index,
+    autopct="%1.1f%%"
+)
+plt.title(
+    "Cơ cấu nhóm khách hàng"
+)
+plt.show()
