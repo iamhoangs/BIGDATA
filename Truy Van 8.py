25b7f9bfc574487acc80b51e929e89ad794c16c1
giamann <120298287+giamann@users.noreply.github.com>
2026-06-07 21:49:41 +0700

    Truy Van 8.py

--- Truy Van 8.py
+++ Truy Van 8.py
@@ -47,6 +47,70 @@ translation = spark.read.csv(
     "hdfs://localhost:9000/Olist/product_category_name_translation.csv",
     header=True,
     inferSchema=True
+)#Kiểm tra giá trị bị thiếu
+from pyspark.sql.functions import *
+datasets = {
+    "customers": customers,
+    "geolocation": geolocation,
+    "order_items": order_items,
+    "payments": payments,
+    "reviews": reviews,
+    "orders": orders,
+    "products": products,
+    "sellers": sellers,
+    "translation": translation
+}
+for name, df in datasets.items():
+    print(f"\n Kết quả kiểm tra giá trị bị thiếu của bảng: {name.upper()}")
+    df.select([
+        count(
+            when(col(c).isNull(), c)
+        ).alias(c)
+        for c in df.columns
+    ]).show()
+
+#Kiểm tra dữ liệu trùng lặp
+datasets = {
+    "customers": customers,
+    "geolocation": geolocation,
+    "order_items": order_items,
+    "payments": payments,
+    "reviews": reviews,
+    "orders": orders,
+    "products": products,
+    "sellers": sellers,
+    "translation": translation
+}
+# Loại bỏ dữ liệu trùng lặp
+for name, df in datasets.items():
+
+    before = df.count()
+
+    after = df.dropDuplicates().count()
+
+    print(
+        f"{name}: Before = {before}, After = {after}"
+    )
+
+#Chuẩn hóa dữ liệu thời gian
+from pyspark.sql.functions import to_timestamp
+
+orders = orders \
+.withColumn(
+    "order_purchase_timestamp",
+    to_timestamp("order_purchase_timestamp")
+) \
+.withColumn(
+    "order_approved_at",
+    to_timestamp("order_approved_at")
+) \
+.withColumn(
+    "order_delivered_customer_date",
+    to_timestamp("order_delivered_customer_date")
+) \
+.withColumn(
+    "order_estimated_delivery_date",
+    to_timestamp("order_estimated_delivery_date")
 )
 customers.createOrReplaceTempView("customers")
 geolocation.createOrReplaceTempView("geolocation")
